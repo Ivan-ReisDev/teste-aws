fd14429f9ad020c66880ef594e682d6d27939190
Ivan Reis <ivanreisdev@gmail.com>
2025-01-21 12:59:32 -0300

    teste atualização

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Atualizei aquiaaaaaaaaaaaaaaaaaaaaaaa';
+    return 'Integração com CI/CD pronta';
   }
 }
